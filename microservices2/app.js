c84ab49a82a38f9abcb0dbe0b9bf6a3e6384e97c
stephenesu <esujoestephen@gmail.com>
2024-08-14 16:20:30 +0100

    Newly modified files

--- microservices2/app.js
+++ microservices2/app.js
@@ -3,10 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Microservice 2 - Node.js API');
+  res.send('Hello from Microservices 2 - Node.js API');
 });
 
 app.listen(port, () => {
-  console.log(`Microservice 2 listening at http://localhost:${port}`);
+  console.log(`Microservices 2 listening at http://localhost:${port}`);
 });
 
